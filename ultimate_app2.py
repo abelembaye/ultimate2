9c20231fdaaf72f4f740c3107d3579540b3b35ab
aembaye <abel.embaye@gmail.com>
2024-01-21 16:39:51 -0600

    Update ultimate_app2.py

--- ultimate_app2.py
+++ ultimate_app2.py
@@ -5,6 +5,4 @@ conn = st.connection('mysql', type='sql')
 # Perform query.
 df = conn.query('SELECT * from mytable;', ttl=600)
 
-# Print results.
-for row in df.itertuples():
-    st.write(f"{row.name} has a :{row.pet}:")
+st.write(df)
