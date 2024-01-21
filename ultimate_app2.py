c4529debb7b2ed13fc7dabf501cba427fc70a19e
aembaye <abel.embaye@gmail.com>
2024-01-21 16:54:32 -0600

    Update ultimate_app2.py

--- ultimate_app2.py
+++ ultimate_app2.py
@@ -3,6 +3,6 @@ import streamlit as st
 conn = st.connection('mysql', type='sql')
 
 # Perform query.
-df = conn.query('SELECT * from mytable;', ttl=600)
+df = conn.query('SELECT * from hw01;', ttl=600)
 
 st.write(df)
