131eab6b27743a2a766115fa394ba7c101249c5c
aembaye <abel.embaye@gmail.com>
2024-01-22 00:29:24 -0600

    Update ultimate_app2.py

--- ultimate_app2.py
+++ ultimate_app2.py
@@ -2,7 +2,7 @@ import streamlit as st
 import sqlalchemy
 
 # conn = st.connection('mysql', type='sql')
-st.experimental_connection
+st.experimental_connection('mysql', type='sql')
 
 # Perform query.
 df = conn.query('SELECT * from hw01;', ttl=0)
